0419edb8f12b679147efaa436fe9ed9973bf88c4
ShyBL <rakesh1985@gmail.com>
2024-07-12 23:52:03 +0300

    Player Movement
    + Input System Package

--- Assets/Shy/Player.cs
+++ Assets/Shy/Player.cs
@@ -1,18 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private PlayerPhysx playerPhysx;
+    
+    [Header(" Movement ")]
+    [SerializeField] private bool canMove = true;
+    [SerializeField] private float moveSpeed = 8f; 
+    [SerializeField] private Vector3 moveInputVector;
+    
+    public static Player Instance;
+    
+    private void Awake()
     {
-        
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+    
+    private void OnEnable()
+    {
+        playerInput.onMove += MovementHandler;
+        playerInput.onMoveStopped += MovementHandler;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        playerInput.onMove -= MovementHandler;
+        playerInput.onMoveStopped -= MovementHandler;
     }
+
+    private void MovementHandler()
+    {
+        if (canMove) 
+        {
+            moveInputVector = playerInput.moveVector;
+
+            playerPhysx.HandleMovement(moveInputVector, moveSpeed);
+        }
+    }
+    
+    public void EnableMovement() => canMove = true;
+    public void DisableMovement() => canMove = false;
+    public void StopInPlace() => playerPhysx.HandleMovement(new Vector3(0,0,0), 0);
+    
 }
