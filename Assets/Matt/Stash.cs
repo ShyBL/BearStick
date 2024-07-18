cffabb0174dec3e6b1ea2269ac73c8a092e96549
MadMatt4000 <matthewlewiscomputers@gmail.com>
2024-07-18 14:02:31 -0400

    Started Stash Refactoring

--- Assets/Matt/Stash.cs
+++ Assets/Matt/Stash.cs
@@ -3,26 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
-public class goalArea : MonoBehaviour
+public class Stash : MonoBehaviour
 {
-    // Simple event that sends the list of pickups on this goal area to the listener.
-    public event EventHandler<List<pickup>> GoalAreaContact;
-
     // Here is a list of pickups, serialized so level designers can add pickups to the list for the goal area.
     [SerializeField] private List<pickup> pickupList;
+    [SerializeField] private PlayerInput playerInput;
 
     // This boolean determines if this goal area has been used or not.
-    private bool triggered = false;
+    private bool inRange = false;
 
     // The player needs a "Player" tag. When the player collides with this object, so long as triggered is false, the event is triggered and the boolean is set.
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && !triggered)
+        if (other.CompareTag("Player"))
         {
-            Debug.Log("You have contacted the Dumpster! and gotten its items.");
-            GoalAreaContact?.Invoke(this, pickupList);
-            triggered = true;
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inRange = false;
+        }
+    }
+
+    private void OnEnable()
+    {
+        playerInput.onInteract += Interact;
+    }
+
+    private void Interact()
+    {
+        if (inRange)
+        {
+
         }
     }
 }
