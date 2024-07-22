79717fc1a7c695dc75f36f21733b00d7f415693a
WarlordSdocy <docystephen@gmail.com>
2024-07-22 16:17:54 -0700

    Refactor to tooltip
    Refactored the tooltip code to be ready for handling dragging items to delete or sell them.

--- Assets/_Underneath/Inventory/Scripts/ItemVisual.cs
+++ Assets/_Underneath/Inventory/Scripts/ItemVisual.cs
@@ -40,46 +40,82 @@ public class ItemVisual : VisualElement
         m_Tooltip.AddToClassList("tooltip");
         AddToClassList("visual-icon-container");
 
-        // Register events for pointer entering and leaving the icon.
+        // Register events for pointer to handle tooltip and dragging items.
         RegisterCallback<PointerOverEvent>(OnPointerOver);
         RegisterCallback<PointerOutEvent>(OnPointerOut);
+        RegisterCallback<PointerDownEvent>(OnPointerDown);
+        RegisterCallback<PointerUpEvent>(OnPointerUp);
+    }
+
+    void OnPointerDown(PointerDownEvent data)
+    {
+
+    }
+
+    void OnPointerUp(PointerUpEvent data)
+    {
+
+    }
+
+    void MoveItem(PointerMoveEvent data)
+    {
+
     }
 
     // When the mouse goes over the inventory slot
     void OnPointerOver(PointerOverEvent data)
     {
-        // Convert moust position from world space to local space of root element, then assign that as the position
-        MoveTooltip(m_Root.WorldToLocal(data.position));
-        // Add the tooltip to the root element so it will render
-        m_Root.Add(m_Tooltip);
-
-        // Register the mouse movment event so the tooltip will move with the mouse
-        RegisterCallback<PointerMoveEvent>(MouseMovementEvent);
+        ShowTooltip(data.position);
     }
 
     // When the mouse leaves the inventory slot
     void OnPointerOut(PointerOutEvent data)
     {
-        // Deregister the move event as we no longer need it
-        UnregisterCallback<PointerMoveEvent>(MouseMovementEvent);
+        HideTooltip();
+    }
+
+    // Shows the tooltip and handles registering the mouse movement event for moving it with the mouse
+    void ShowTooltip(UnityEngine.Vector2 pos)
+    {
+        // Convert moust position from world space to local space of root element, then assign that as the position
+        MoveTooltip(m_Root.WorldToLocal(pos));
+        // Add the tooltip to the root element so it will render
+        m_Root.Add(m_Tooltip);
+
+        // Register the mouse movment event so the tooltip will move with the mouse
+        RegisterCallback<PointerMoveEvent>(TooltipMouseMovement);
+    }
+
+    // Function that actually removes the tooltip and gets rid of the mouse movement callback event
+    void HideTooltip()
+    {
+        // Deregister the move event as we no longer need it since tooltip will be hidden
+        UnregisterCallback<PointerMoveEvent>(TooltipMouseMovement);
 
         // Remove the tooltip from the root element so it no longer renders
         m_Root.Remove(m_Tooltip);
     }
 
-    // When the mouse moves, only registered when the mouse is hovering over the inventory slot
-    void MouseMovementEvent(PointerMoveEvent data)
+    // When the mouse moves this is called to move the tooltip, only registered when the mouse 
+    // is hovering over the inventory slot and when the player isn't dragging an item.
+    void TooltipMouseMovement(PointerMoveEvent data)
     {
         // Convert moust position from world space to local space of root element, then assign that as the position
         MoveTooltip(m_Root.WorldToLocal(data.position));
     }
 
-    // Moves the tooltip based on the provided position, assumed position is in local space of the root element
+    // Function for moving the tooltip to the passed position, use this instead of move item for moving tooltip
     void MoveTooltip(UnityEngine.Vector2 pos)
     {
         // Have to have + 1 here so that the tooltip doesn't steal the mouse cursor and cause
         // the pointer out function above to be called too early
-        m_Tooltip.style.left = pos.x + 1;
-        m_Tooltip.style.top = pos.y + 1;
+        MoveItem(m_Tooltip, pos + new UnityEngine.Vector2(1, 1));
+    }
+
+    // Moves the tooltip based on the provided position, assumed position is in local space of the root element
+    void MoveItem(VisualElement item, UnityEngine.Vector2 pos)
+    {
+        item.style.left = pos.x + 1;
+        item.style.top = pos.y + 1;
     }
 }
