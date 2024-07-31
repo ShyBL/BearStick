00c7dd56623cffe1b348730df2c1f80317a51f67
Near <nearsmash@gmail.com>
2024-07-31 15:43:07 -0500

    wallSlide/Jump

--- Assets/_Underneath/Player/Scripts/Player.cs
+++ Assets/_Underneath/Player/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
     [SerializeField] public PlayerInput playerInput;
     [SerializeField] private PlayerPhysx playerPhysx;
     [SerializeField] private PlayerVisualizer playerVisualizer;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private SpriteRenderer sr;
     
     [Header(" Movement ")]
     [SerializeField] private float moveSpeed = 8f;
@@ -13,9 +15,22 @@ public class Player : MonoBehaviour
     [SerializeField] private float jumpForce = 15;
     [SerializeField] private bool canMove = true;
     [SerializeField] private Vector3 moveInputVector;
+    private bool grounded;
     
     public static Player Instance;
     [SerializeField] public Inventory Inventory;
+
+    [Header("Wall Mechanics")]
+    private bool isWallSliding = false;
+    private bool isWallJumping = false;
+    private float wJumpDir;
+    private float wJumpCounter;
+    [SerializeField] private float wJumpTime;
+    [SerializeField] private float wJumpDur;
+    [SerializeField] private Vector2 wJumpForce;
+    [SerializeField] private float wSlideSpeed;
+    [SerializeField] private Transform wCheck, gCheck;
+    [SerializeField] private LayerMask wLayer, gLayer;
 
     private void Awake()
     {
@@ -67,6 +82,20 @@ public class Player : MonoBehaviour
                 StopMovementHandler();
             }
         }
+
+        wallSlide();
+        wallJump();
+
+        grounded = isGrounded();
+    }
+
+    private void FixedUpdate()
+    {
+        if (grounded)
+        {
+            //Debug.Log("groundStop");
+            stopWallJump();
+        }
     }
 
     private void StopMovementHandler()
@@ -77,16 +106,22 @@ public class Player : MonoBehaviour
 
     private void MovementHandler()
     {
-        Flip();
-        playerVisualizer.SetRun();
+        if (!isWallJumping)
+        {
+            Flip();
+            playerVisualizer.SetRun();
         
-        playerPhysx.HandleMovement(moveInputVector, moveSpeed);
+            playerPhysx.HandleMovement(moveInputVector, moveSpeed);
+        }
     }
  
     private void JumpingHandler()
     {
-        playerVisualizer.SetJump();
-        playerPhysx.Jump(moveInputVector, airVelocity, jumpForce);
+        if (grounded)
+        {
+            playerVisualizer.SetJump();
+            playerPhysx.Jump(moveInputVector, airVelocity, jumpForce);
+        }
 
         // if (isGrounded())
         // {
@@ -105,9 +140,17 @@ public class Player : MonoBehaviour
         //     transform.localScale = new Vector3(1 * facingDirection, 1, 1);
         // }
         
-        if (moveInputVector.x != 0)
+        if (moveInputVector.x != 0 && !isWallJumping)
         {
             playerVisualizer.spriteComponent.flipX = moveInputVector.x > 0;
+            if (moveInputVector.x > 0)
+            {
+                wCheck.localPosition = new Vector3(0.25f, 0f, 0f);
+            }
+            else if (moveInputVector.x < 0)
+            {
+                wCheck.localPosition = new Vector3(-0.25f, 0f, 0f);
+            }
         }
     }
     
@@ -117,6 +160,86 @@ public class Player : MonoBehaviour
     
     public void Jump() => playerPhysx.Jump(moveInputVector, airVelocity, jumpForce);
     public Vector3 Velocity() => playerPhysx.CurrentVelocity();
-    public bool isGrounded() => playerPhysx.IsGrounded;
+    //public bool isGrounded() => playerPhysx.IsGrounded;
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(gCheck.position, 0.1f, gLayer);
+    }
+
+    private bool isWalled()
+    {
+        return Physics2D.OverlapCircle(wCheck.position, 0.2f, wLayer);
+    }
+
+    private void wallSlide()
+    {
+        //Debug.Log(isWalled() + ", " + isGrounded() + ", " + moveInputVector.x);
+        if (isWalled() && !isGrounded() && moveInputVector.x != 0)
+        {
+            //Debug.Log("wallDetect");
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wSlideSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    private void wallJump()
+    {
+        if (isWallSliding)
+        {
+            isWallJumping = false;
+            wJumpDir = -moveInputVector.x;
+            wJumpCounter = wJumpTime;
+
+            //CancelInvoke(nameof(stopWallJump));
+        }
+        else if (grounded)
+        {
+            wJumpCounter = wJumpTime;
+            isWallJumping = false;
+            //CancelInvoke(nameof(stopWallJump));
+        }
+        else
+        {
+            wJumpCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump") && wJumpCounter > 0f && !isWallJumping && isWalled())
+        {
+            canMove = false;
+            isWallJumping = true;
+            StopInPlace();
+            moveInputVector = new Vector3(0f, 0f, 0f);
+
+            rb.velocity = new Vector2(wJumpDir * wJumpForce.x, wJumpForce.y);
+            wJumpCounter = 0f;
+
+            if (moveInputVector.x != wJumpDir)
+            {
+                sr.flipX = !sr.flipX;
+                if (sr.flipX)
+                {
+                    wCheck.localPosition = new Vector3(0.25f, 0f, 0f);
+                }
+                else if (!sr.flipX)
+                {
+                    wCheck.localPosition = new Vector3(-0.25f, 0f, 0f);
+                }
+            }
+
+            //Invoke(nameof(stopWallJump), wJumpDur);
+        }
+    }
+
+    private void stopWallJump()
+    {
+        Debug.Log("wjStopped");
+        isWallJumping = false; 
+        canMove = true;
+    }
     
 }
