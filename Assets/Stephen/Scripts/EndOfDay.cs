1e02f2ebd5986fe580deb580591d19c89d41c1d3
ShyBL <rakesh1985@gmail.com>
2024-09-09 01:07:22 +0300

    Mosh branches manual import

--- Assets/Stephen/Scripts/EndOfDay.cs
+++ Assets/Stephen/Scripts/EndOfDay.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class EndOfDay : MonoBehaviour
@@ -28,7 +27,6 @@ public class EndOfDay : MonoBehaviour
     private Label m_ExpensesLabel;
     private Label m_Title;
 
-
     void Start()
     {
         m_Document = GetComponent<UIDocument>();
@@ -40,19 +38,11 @@ public class EndOfDay : MonoBehaviour
         m_Document.rootVisualElement.Q<Button>("Exit").RegisterCallback<ClickEvent>(ExitPressed);
         PlayerData.Instance.IncreaseExpenses(BaseExpenses);
         m_Document.rootVisualElement.style.display = DisplayStyle.None;
-
-        
     }
 
     void ExitPressed(ClickEvent evt)
     {
         m_Document.rootVisualElement.style.display = DisplayStyle.None;
-        //when this is clicked, need to load a new day
-        //StartOfDay.Instance.StartNewDay();
-
-        //This is how you would reset the scene, but you would need some form of button or something 
-        //So that the scene does not instantly reset
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void EndDay()
@@ -63,6 +53,9 @@ public class EndOfDay : MonoBehaviour
         m_PrevMoneyLabel.text = "$" + PlayerData.Instance.GetMoney().ToString();
         m_EarnedMoneyLabel.text = "$" + PlayerData.Instance.GetMoneyEarned().ToString();
 
+        if (PlayerData.Instance.GetDayCount() % IncreaseFrequency == 0)
+            PlayerData.Instance.IncreaseExpenses(ExpensesIncrease);
+
         PlayerData.Instance.DecreaseMoney(PlayerData.Instance.GetExpenses());
         PlayerData.Instance.ApplyMoneyChange();
 
@@ -70,15 +63,5 @@ public class EndOfDay : MonoBehaviour
         m_TotalMoneyLabel.text = "$" + PlayerData.Instance.GetMoney().ToString();
 
         PlayerData.Instance.IncrementDayCount();
-
-        if (PlayerData.Instance.GetDayCount() % IncreaseFrequency == 0)
-            PlayerData.Instance.IncreaseExpenses(ExpensesIncrease);
-
-        //Set the player's spawn location for the next day
-        Player.Instance.SetPlayerSpawn(PlayerData.Instance.v_SpawnLocation);
-        //Save Game
-        SavingAndLoading.Instance.SavePlayerInformation();
-;
     }
-
 }
