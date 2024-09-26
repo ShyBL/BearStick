817212c93bc39bb1e7c1a3774c2356eaf109a203
ShyBL <rakesh1985@gmail.com>
2024-09-27 01:11:12 +0300

    Test Controller
    ULTIMATE 2D Platformer Controller for Unity - https://youtu.be/zHSWG05byEc?si=fPwHac0jTV4e4Mac

---
+++ Assets/_Underneath/Tests/InputManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class InputManager
+{
+    public static bool RunIsHeld { get; set; }
+    public static Vector2 Movement { get; set; }
+    public static bool JumpWasPressed { get; set; }
+    public static bool JumpWasReleased { get; set; }
+}
