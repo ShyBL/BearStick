e04608d154d64b705a5d8fe17cf897912b3c79a0
ShyBL <rakesh1985@gmail.com>
2024-11-01 00:20:59 +0200

    Changed Item class
    Bug- UI was using the texture, which was sampling the sprite sheet instead of the sprite.
    Solution- Added a new Sprite variable to be used by the collectible physical object, and changed the icons to use a single sprite.
    
    + Reordered new sprites

--- Assets/_Underneath/Game Item/Scripts/PhysicsCollectible.cs
+++ Assets/_Underneath/Game Item/Scripts/PhysicsCollectible.cs
@@ -32,6 +32,6 @@ public class PhysicsCollectible : MonoBehaviour
     {
         collectable = inputCollectable;
 
-        GetComponent<SpriteRenderer>().sprite = collectable.Icon;
+        GetComponent<SpriteRenderer>().sprite = collectable.Collectable;
     }
 }
