15222c7e8f63b47427a3063f845fb51ebdff06d6
ShyBL <rakesh1985@gmail.com>
2024-11-11 01:15:37 +0200

    Parallax
    + statue sprite

--- Assets/Shy/Scripts/ParallaxLayer.cs
+++ Assets/Shy/Scripts/ParallaxLayer.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
-    public class ParallaxLayer : MonoBehaviour
+
+public class ParallaxLayer : MonoBehaviour
+{
+    [SerializeField] private Vector3 movementScale = Vector3.one;
+    [SerializeField] private Camera _camera;
+    private Vector3 initialPosition;
+
+    void Start()
     {
-        [SerializeField] private Vector3 movementScale = Vector3.one;
-
-        Transform _camera;
-
-        void Awake()
-        {
-            _camera = Camera.main.transform;
-        }
-
-        void LateUpdate()
-        {
-            transform.position = Vector3.Scale(_camera.position, movementScale);
-        }
+        initialPosition = transform.position;
     }
+
+    void LateUpdate()
+    {
+        Vector3 cameraMovement = Vector3.Scale(_camera.transform.position, movementScale);
+        Vector3 newPosition = initialPosition + new Vector3(cameraMovement.x, 0, 0); 
+        transform.position = newPosition;
+    }
+}
