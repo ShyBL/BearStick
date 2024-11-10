56a822469888d0f0f7c39f32b0137d9d8e523d5d
ShyBL <rakesh1985@gmail.com>
2024-11-10 08:19:01 +0200

    Warehouse with lights
    temporary assets

---
+++ Assets/Shy/ParallaxLayer.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+    public class ParallaxLayer : MonoBehaviour
+    {
+        [SerializeField] private Vector3 movementScale = Vector3.one;
+
+        Transform _camera;
+
+        void Awake()
+        {
+            _camera = Camera.main.transform;
+        }
+
+        void LateUpdate()
+        {
+            transform.position = Vector3.Scale(_camera.position, movementScale);
+        }
+    }
