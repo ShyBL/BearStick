2648317830d4a20b961a12e4246cfe76e8fc391c
Blechman Shy (NorthStar) <rakesh1985@gmail.com>
2024-12-27 21:35:30 +0200

    Save file as scriptable object

--- Assets/_Underneath/UI/Scripts/GameplayManager.cs
+++ Assets/_Underneath/UI/Scripts/GameplayManager.cs
@@ -16,12 +16,13 @@ public class GameplayManager : OurMonoBehaviour
     public delegate void RefreshMoney();
     public RefreshMoney m_RefreshMoney;
     
-    public SavedPlayerData PlayerData;
-
+    //public SavedPlayerData PlayerData;
+    public SavePlayerData PlayerData;
     private void Awake()
     {
         if (Instance == null)
         {
+            InitializeFromSave();
             Instance = this;
         }
         else if (Instance != this)
@@ -29,28 +30,35 @@ public class GameplayManager : OurMonoBehaviour
             Destroy(this);
         }
         
-        InitializeFromSave();
     }
 
     private void InitializeFromSave()
     {
-        PlayerData = GameManager.SaveManager.LoadDataAndCreateIfNull<SavedPlayerData>();
+        PlayerData = Resources.Load<SavePlayerData>("SavePlayerData");
         
-        foreach (var kvp in PlayerData.SavedPlayerDateDic)
-        {
-            switch (kvp.Key)
-            {
-                case "Money":
-                    Money = kvp.Value;
-                    break;
-                case "DayCount":
-                    DayCount = kvp.Value;
-                    break;
-                case "Expenses": 
-                    Expenses = kvp.Value;
-                    break;
-            }
-        }
+        PlayerData.Money = Money;
+        PlayerData.Expenses = Expenses;
+        PlayerData.DayCount = DayCount;
+        PlayerData.TempMoneyValue = TempMoneyValue;
+        PlayerData.v_SpawnLocation = v_SpawnLocation;
+        
+        // PlayerData = GameManager.SaveManager.LoadDataAndCreateIfNull<SavedPlayerData>();
+        //
+        // foreach (var kvp in PlayerData.SavedPlayerDateDic)
+        // {
+        //     switch (kvp.Key)
+        //     {
+        //         case "Money":
+        //             Money = kvp.Value;
+        //             break;
+        //         case "DayCount":
+        //             DayCount = kvp.Value;
+        //             break;
+        //         case "Expenses": 
+        //             Expenses = kvp.Value;
+        //             break;
+        //     }
+        // }
     }
 
     private void Start()
@@ -59,75 +67,90 @@ public class GameplayManager : OurMonoBehaviour
     
         if (spawnLocation != null)
         {
-            v_SpawnLocation = spawnLocation.transform.position;
+           // v_SpawnLocation = spawnLocation.transform.position;
+           PlayerData.v_SpawnLocation = spawnLocation.transform.position;
+
         }
         else
         {
             GameObject newSpawnLocation = new GameObject("PlayerRespawnPoint");
             newSpawnLocation.transform.position = this.transform.position;
-            v_SpawnLocation = newSpawnLocation.transform.position;
+          //  v_SpawnLocation = newSpawnLocation.transform.position;
+
+          PlayerData.v_SpawnLocation = newSpawnLocation.transform.position;
         }
     }
 
     private void IncreaseMoney(int amount)
     {
-        TempMoneyValue += amount; 
+       // TempMoneyValue += amount; 
+       PlayerData.TempMoneyValue += amount;
     }
 
     public void DecreaseMoney(int amount)
     {
-        TempMoneyValue -= amount;
+      //  TempMoneyValue -= amount;
+      PlayerData.TempMoneyValue -= amount;
     }
 
     public void IncrementDayCount()
     {
-        DayCount++;
-        if (PlayerData.SavedPlayerDateDic.ContainsKey("DayCount"))
-        {
-            PlayerData.SavedPlayerDateDic["DayCount"] = DayCount;
-        }
-        PlayerData.SaveData();
+        PlayerData.DayCount++;
+      //  DayCount++;
+        // if (PlayerData.SavedPlayerDateDic.ContainsKey("DayCount"))
+        // {
+        //     PlayerData.SavedPlayerDateDic["DayCount"] = DayCount;
+        // }
+        //PlayerData.SaveData();
     }
     public void IncreaseExpenses(int amount)
     {
-        Expenses += amount;
-        if (PlayerData.SavedPlayerDateDic.ContainsKey("Expenses"))
-        {
-            PlayerData.SavedPlayerDateDic["Expenses"] = Expenses;
-        }
-        PlayerData.SaveData();
+        PlayerData.Expenses += amount;
+        // Expenses += amount;
+        // if (PlayerData.SavedPlayerDateDic.ContainsKey("Expenses"))
+        // {
+        //     PlayerData.SavedPlayerDateDic["Expenses"] = Expenses;
+        // }
+        //PlayerData.SaveData();
     }
 
     public void ApplyMoneyChange()
     {
-        Money += TempMoneyValue;
-        TempMoneyValue = 0;
+        PlayerData.Money += PlayerData.TempMoneyValue;
+        PlayerData.TempMoneyValue = 0;
         m_RefreshMoney.Invoke();
-        if (PlayerData.SavedPlayerDateDic.ContainsKey("Money"))
-        {
-            PlayerData.SavedPlayerDateDic["Money"] = Money;
-        }
-        PlayerData.SaveData();
+        // Money += TempMoneyValue;
+        // TempMoneyValue = 0;
+        // m_RefreshMoney.Invoke();
+        // if (PlayerData.SavedPlayerDateDic.ContainsKey("Money"))
+        // {
+        //     PlayerData.SavedPlayerDateDic["Money"] = Money;
+        // }
+        //PlayerData.SaveData();
     }
 
     public int GetMoney()
     {
-        return Money;
+        return PlayerData.Money;
+        // return Money;
     }
 
     public int GetDayCount()
     {
-        return DayCount;
+        return PlayerData.DayCount;
+       // return DayCount;
     }
 
     public int GetExpenses()
     {
-        return Expenses;
+        return PlayerData.Expenses;
+       // return Expenses;
     }
 
     public int GetMoneyEarned()
     {
-        return TempMoneyValue;
+        return PlayerData.Expenses;
+       // return TempMoneyValue;
     }
 
     public void ShopPayoff()
@@ -147,4 +170,13 @@ public class SavedPlayerData : ISaveData
     {
         {"Money",0}, {"DayCount",1}, {"Expenses",20}
     };
+}
+[CreateAssetMenu(fileName = "SavedPlayerData", menuName = "SavedPlayerData")]
+public class SavePlayerData : ScriptableObject
+{
+    public int Money;
+    public int TempMoneyValue;
+    public int DayCount;
+    public int Expenses;
+    public Vector2 v_SpawnLocation;
 }
